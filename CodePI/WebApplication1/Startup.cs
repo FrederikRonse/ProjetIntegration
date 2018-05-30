ccdf97a34685f6bbde63d69cc2c4c499f8621ce0
FrederikRonse <31375140+FrederikRonse@users.noreply.github.com>
2018-05-30 20:44:27 +0200

    Structure générale de la solution
    Après avoir exploré différentes options, je pars sur cette achitecture.
    J'ai  pour le moment laissé une connection à la DB, avec login sa et pwd en clair, pour la gestion des authentication par MVC (identityModel)..

---
+++ CodePI/WebApplication1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1.Startup))]
+namespace WebApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            //services.AddEntityFramework().AddSqlServer().AddDbContext(options => options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+        }
+    }
+}
